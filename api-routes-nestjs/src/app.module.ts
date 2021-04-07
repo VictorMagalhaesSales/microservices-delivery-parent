b5fe411123feb323b9cb656fa59975699d6c5d6e
victormagalhaessales <victormagalhaessales@gmail.com>
2021-04-07 14:23:09 -0300

    Implementing routes resource

--- api-routes-nestjs/src/app.module.ts
+++ api-routes-nestjs/src/app.module.ts
@@ -1,10 +1,40 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RoutesController } from './controllers/routes.controller';
+import { Route, RouteSchema } from './entities/route.entity';
+import { RoutesGateway } from './gateways/routes.gateway';
+import { RoutesService } from './services/routes.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_DSN, {useNewUrlParser: true}),
+    MongooseModule.forFeature([{ name: Route.name, schema: RouteSchema }]),
+    ClientsModule.registerAsync([
+      {
+        name: 'KAFKA_SERVICE',
+        useFactory: () => ({
+          transport: Transport.KAFKA,
+          options: {
+            client: {
+              clientId: process.env.KAFKA_CLIENT_ID,
+              brokers: [process.env.KAFKA_BROKER],
+            },
+            consumer: {
+              groupId:
+                !process.env.KAFKA_CONSUMER_GROUP_ID ||
+                process.env.KAFKA_CONSUMER_GROUP_ID === ''
+                  ? 'my-consumer-' + Math.random()
+                  : process.env.KAFKA_CONSUMER_GROUP_ID,
+            },
+          },
+        }),
+      },
+    ])
+  ],
+  controllers: [RoutesController],
+  providers: [RoutesService, RoutesGateway]
 })
 export class AppModule {}
